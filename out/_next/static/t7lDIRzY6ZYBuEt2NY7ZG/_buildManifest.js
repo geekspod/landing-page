681b63490cab68dcb3291f91e0e947611c85aec4
Awais <awaisghaffar77@gmail.com>
2022-11-21 13:20:13 -0500

    Updated outfolder to test

--- out/_next/static/t7lDIRzY6ZYBuEt2NY7ZG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-33f37a3ac494e84d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
