484801701234f05585404fb5816de0a621f9775c
Awais <awaisghaffar77@gmail.com>
2022-11-22 14:04:10 -0500

    Fixed the content for companies page

--- src/components/WGSHeader.jsx
+++ src/components/WGSHeader.jsx
@@ -14,10 +14,9 @@ export function WGSHeader() {
             </div>
             <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
                 <div className="md:ml-auto md:w-1/2 md:pl-10">
-                    <p className="mt-2 text-white text-xl font-bold tracking-tight lg:text-3xl">Simplify your checkout process.</p>
+                    <p className="mt-2 text-white text-xl font-bold tracking-tight lg:text-3xl">Have an up-&-running organization, or an up-&-coming startup that's ready to disrupt the competition?</p>
                     <p className="mt-3 text-sm lg:text-lg text-gray-300">
-                        GAPP Blocks is a scalable, easy-to-use and fast way to set up merchants with an eCommerce store. Now you can sell anywhere without the hassle of third-party payment gateways: just integrate once, get your store set up and selling in one click, and enjoy easy payment processing for any mobile device or desktop. 
-                    </p>
+                    you'll need a team that aligns with your goals. Well,we are here to help. Our vetting and scrutiny processes make sure that we hire the perfect geeks for you! So that all that's left for you to do, is consult us.                    </p>
                     <div className="mt-8">
 
                     </div>
