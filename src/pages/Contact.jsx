e59a5801529fd89c811d2baec014baed4b7ba478
Awais <awaisghaffar77@gmail.com>
2022-12-05 10:24:42 -0500

    add description and lang meta tags

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,9 @@ export default function Home() {
       <>
       <Head>
       <meta charset="UTF-8" />
-      <meta name="keywords" content="recruitment, IT consultant, consultant,careers,career opportunities,jobs,geeks,software developer,engineering,organization,jobs hiring,work from home,remote jobs,team building,part time jobs,employment,job search,software engineer,jobs hiring,career opportunities,jobs near me,jobs hiring near me,part time jobs near me,online jobs,job search," />
+      <meta name="keywords" content="recruitment, IT consultant, remote jobs hiring,full time jobs,consultant,careers,consultants,part time remote jobs,IT recruitment,corporation,hiring,job sites,business,business opportunities,candidates,career opportunities,computer engineering,job opportunities,jobs,work from home jobs,find a job,target hiring,geeks,software developer,professionals,full stack engineer,engineering team,engineering,organization,jobs hiring,work from home,remote jobs,team building,part time jobs,employment,job search,software engineer,jobs hiring,career opportunities,jobs near me,jobs hiring near me,part time jobs near me,online jobs,job search," />
+      <meta lang="en" />
+<meta name="description" content="GeeksPod - Lets Connect" />  
       <title>GeeksPod - Let&apos;s Connect</title>
       <link rel="shortcut icon" href={favicon.src} />
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,9 @@ export default function Home() {
 
 <Head>
 <meta charset="UTF-8" />
-      <meta name="keywords" content="recruitment, IT consultant, remote jobs hiring,full time jobs,consultant,careers,consultants,part time remote jobs,IT recruitment,corporation,hiring,job sites,business,business opportunities,candidates,career opportunities,computer engineering,job opportunities,jobs,work from home jobs,find a job,target hiring,geeks,software developer,professionals,full stack engineer,engineering team,engineering,organization,jobs hiring,work from home,remote jobs,team building,part time jobs,employment,job search,software engineer,jobs hiring,career opportunities,jobs near me,jobs hiring near me,part time jobs near me,online jobs,job search," />
+<meta lang="en" />
+<meta name="description" content="GeeksPod - Build Your Dream Engineering Team" />
+<meta name="keywords" content="recruitment, IT consultant, remote jobs hiring,full time jobs,consultant,careers,consultants,part time remote jobs,IT recruitment,corporation,hiring,job sites,business,business opportunities,candidates,career opportunities,computer engineering,job opportunities,jobs,work from home jobs,find a job,target hiring,geeks,software developer,professionals,full stack engineer,engineering team,engineering,organization,jobs hiring,work from home,remote jobs,team building,part time jobs,employment,job search,software engineer,jobs hiring,career opportunities,jobs near me,jobs hiring near me,part time jobs near me,online jobs,job search," />
 <title>GeeksPod - Build Your Dream Engineering Team</title>
 <link rel="shortcut icon" href={favicon.src} />
 </Head>
